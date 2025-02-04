9e6c868e5d62da9d6507bed58967de064cbd218e
sarovar12 <85166131+sarovar12@users.noreply.github.com>
2025-02-04 13:40:25 +0545

    feat: Add migration files for table modification

---
+++ Crop.Backend/Migrations/20250204075445_ModifyCropRecommendationTbl.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Course.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifyCropRecommendationTbl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Recommendation",
+                table: "CropRecommendations",
+                newName: "RandomForestRecommendation");
+
+            migrationBuilder.AddColumn<string>(
+                name: "GradientBoostingRecommendation",
+                table: "CropRecommendations",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GradientBoostingRecommendation",
+                table: "CropRecommendations");
+
+            migrationBuilder.RenameColumn(
+                name: "RandomForestRecommendation",
+                table: "CropRecommendations",
+                newName: "Recommendation");
+        }
+    }
+}
